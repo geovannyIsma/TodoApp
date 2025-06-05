fbed6c07ae556d999191a01a4de0160a57b768e8
Geovanny Romero <geovanny.romero@unl.edu.ec>
2025-06-04 19:38:19 -0500

    first commit

---
+++ todo/tasks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'tasks'
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('task/create/', views.task_create, name='task_create'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
+    path('task/<int:pk>/update/', views.task_update, name='task_update'),
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+    path('task/<int:pk>/complete/', views.task_complete, name='task_complete'),
+]
